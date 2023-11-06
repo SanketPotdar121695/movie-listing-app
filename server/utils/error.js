6dd8993e0833c96b8a37228d390ca1e271383d82
SanketPotdar121695 <sanket.potdar563@gmail.com>
2023-11-06 23:21:31 +0530

    done setup proxy handlers on both client and server side & deleted the unnecessary directories

--- server/utils/error.js
+++
@@ -1,7 +0,0 @@
-export const errorHandler = (statusCode, message, description) => {
-  const error = new Error();
-  error.statusCode = statusCode;
-  error.message = message;
-  error.description = description;
-  return error;
-};
